0949b5de8aa2c88b8ce7751f556a5e4683ea6196
StarCastFilms <totallygamerjet@gmail.com>
2015-08-14 11:35:43 -0400

    Extra space

--- src/main/java/net/cosmosmc/mcze/events/PlayerInfectedEvent.java
+++ src/main/java/net/cosmosmc/mcze/events/PlayerInfectedEvent.java
@@ -3,7 +3,7 @@ package net.cosmosmc.mcze.events;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public final class PlayerInfectedEvent  extends Event {
+public final class PlayerInfectedEvent extends Event {
 
     private static final HandlerList HANDLERS = new HandlerList();
 
