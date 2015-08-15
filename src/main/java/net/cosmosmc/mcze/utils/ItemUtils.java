1a6f0a00c705344e32f85f21275dc02faeec8561
DonkeyCore <doomsdaysurvivor2013@gmail.com>
2015-08-15 16:21:29 -0500

    Optimized ItemUtils class and added convenience methods

--- src/main/java/net/cosmosmc/mcze/utils/ItemUtils.java
+++ src/main/java/net/cosmosmc/mcze/utils/ItemUtils.java
@@ -9,13 +9,25 @@ import java.util.Arrays;
 //We can use this for inventories and what not
 public class ItemUtils {
 
+    public static ItemStack createItem(Material material, String name, String... lore) {
+        return createItem(material, name, 1, lore);
+    }
+
+    public static ItemStack createItem(Material material, String name, int amount, String... lore) {
+        return createItem(material, name, amount, (short) 0, lore);
+    }
+
     //Feel free to modify this class if needed
-    public static ItemStack createItem(Material material, String name, String[] lore, int amount, short data) {
+    public static ItemStack createItem(Material material, String name, int amount, short data, String... lore) {
         ItemStack item = new ItemStack(material, amount, data);
         ItemMeta meta = item.getItemMeta();
 
-        meta.setDisplayName(name);
-        meta.setLore(Arrays.asList(lore));
+        if(name != null) {
+            meta.setDisplayName(name);
+        }
+        if(lore != null && lore.length > 0) {
+            meta.setLore(Arrays.asList(lore));
+        }
 
         item.setItemMeta(meta);
         return item;
