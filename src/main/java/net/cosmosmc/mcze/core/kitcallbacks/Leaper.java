71dd04f1726e895b7a13a0253d350ff060220942
Julian v.d Berkmortel <julianvdberkmortel@outlook.com>
2015-09-20 04:37:20 +0200

    Added the leaper cooldown.

--- src/main/java/net/cosmosmc/mcze/core/kitcallbacks/Leaper.java
+++ src/main/java/net/cosmosmc/mcze/core/kitcallbacks/Leaper.java
@@ -7,7 +7,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 public class Leaper implements KitAction {
+    private static final int COOLDOWN_TIME = 10; // Seconds
+    private final Map<UUID, Long> LAST_USED = new HashMap<>();
 
     @Override
     public void giveKit(Player player) {
@@ -16,8 +23,14 @@ public class Leaper implements KitAction {
 
     @Override
     public void interact(PlayerInteractEvent event, ItemStack itemStack) {
-        // if time has passed, then allow
-        // otherwise, cancel
-    }
+        Player player = event.getPlayer();
 
+        if(!LAST_USED.containsKey(player.getUniqueId()) || ((System.currentTimeMillis() - LAST_USED.get(player.getUniqueId())) > TimeUnit.SECONDS.toMillis(COOLDOWN_TIME))) {
+            /* TODO: Make the leaper kit work. */
+
+            LAST_USED.put(player.getUniqueId(), System.currentTimeMillis());
+        } else {
+            event.setCancelled(true);
+        }
+    }
 }
